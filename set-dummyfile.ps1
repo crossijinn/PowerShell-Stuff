4a5c2d02f68f0365cad69bdd9fcb450dd6c24c96
crossijinn <50312174+crossijinn@users.noreply.github.com>
2019-06-10 16:57:38 +0200

    Update set-dummyfile.ps1

--- set-dummyfile.ps1
+++ set-dummyfile.ps1
@@ -9,20 +9,20 @@ function Set-DummyFile {
     Process{
         
         #Size in GB umrechnen
-        $size=$size * 1073741824
+        $filesize=$size * 1073741824
 
         #Prüfen ob $Destination exisiertiert, falls nicht wird der Ordner angelegt
         if(-not (Test-Path $destination -PathType Container)){$null =mkdir $destination}
 
         #Anpassen der $number
         $number=$number+1
-        
+
         #Wechsel ins richtige Arbeitsverzeichnis
         Set-Location -Path $destination
 
         #Anlage der Dummy File(s)
         for ($i = 1; $i -lt $number; $i++){
-        fsutil.exe file createnew $name-$i $size
+        fsutil.exe file createnew $name-$i $filesize
     }
     }
 }
